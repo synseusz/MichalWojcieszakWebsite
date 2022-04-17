093021fc730a8d2a50bd9703547815239d1512dc
synseusz <thesynseusz@gmail.com>
2022-04-17 15:04:16 +0200

    messenger customer chat changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import { BrowserRouter as Router } from 'react-router-dom'
 import Home from './pages';
-import MessengerCustomerChat from "react-messenger-customer-chat";
+//import MessengerCustomerChat from "react-messenger-customer-chat";
 
 function App() {
   return (
     <Router>
-      <MessengerCustomerChat pageId="753206974730152" appId="715051529518972" />
+      {/*<MessengerCustomerChat pageId="753206974730152" appId="715051529518972" />*/}
       <Home />
     </Router>
   );
